4410e5447755d1173c38583c937a0093a05328f7
yassinesecref88-a11y <yassinesecref88@gamil.com>
2026-02-18 20:13:53 +0100

    Integrating the H2 dependancy in the app for the devops piplene issues

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,8 +2,11 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
+
 class StudentManagementApplicationTests {
 
     @Test
